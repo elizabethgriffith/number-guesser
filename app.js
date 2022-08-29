f82b53b3a45b49285e823b121e0260b1abaf6c49
Elizabeth Griffith <elizabethgriffith97@gmail.com>
2022-08-29 15:47:20 -0400

    Add saveNumOfGuesses function to store user input

---
+++ app.js
@@ -0,0 +1,32 @@
+// Game Values
+let min = 1,
+    max = 10,
+    winningNumber = randomNumber(min, max),
+    guessesLeft;
+
+// UI Variables
+const numOfGuesses = document.querySelector('#numOfGuesses')
+      numSubmitBtn = document.querySelector('#numSubmitBtn')
+
+function randomNumber(min, max){
+  return Math.floor(Math.random()*(max-min+1) + min)
+}
+
+// Load Event Listeners
+numSubmitBtn.addEventListener('click', saveNumOfGuesses)
+
+
+
+// Create function to check for correct inputs
+
+// Store numOfGuesses input
+function saveNumOfGuesses(e){
+  if (numOfGuesses.value === 'oneGuess'){
+    guessesLeft = 1
+  } else if (numOfGuesses.value === 'twoGuesses'){
+    guessesLeft = 2
+  } else {
+    guessesLeft = 3
+  }
+  console.log(guessesLeft)
+}
