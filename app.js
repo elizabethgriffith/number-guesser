1a445c5f624274b278f26f203a4f993ec49d17e0
Elizabeth Griffith <elizabethgriffith97@gmail.com>
2022-08-29 17:29:23 -0400

    Create function to validate guess and check guess

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ let min = 1,
 // UI Variables
 const numOfGuesses = document.querySelector('#numOfGuesses')
       numSubmitBtn = document.querySelector('#numSubmitBtn')
+      guessBtn = document.querySelector('#guessBtn')
+      guessInput = document.querySelector('#guessInput')
+      message = document.querySelector('.message')
 
 function randomNumber(min, max){
   return Math.floor(Math.random()*(max-min+1) + min)
@@ -14,18 +17,16 @@ function randomNumber(min, max){
 
 // Load Event Listeners
 numSubmitBtn.addEventListener('click', saveNumOfGuesses)
-
-
-
-// Create function to check for correct inputs
+guessBtn.addEventListener('click', validateGuess)
 
 // Store numOfGuesses input
 function saveNumOfGuesses(e){
+  // Validate inputs
   if (numOfGuesses.value === 'select'){
     alert("You must select one, two, or three!")
   }
 
-
+  // Store input into guessesLeft
   if (numOfGuesses.value === 'oneGuess'){
     guessesLeft = 1
   } else if (numOfGuesses.value === 'twoGuesses'){
@@ -34,4 +35,43 @@ function saveNumOfGuesses(e){
     guessesLeft = 3
   }
   console.log(guessesLeft)
+
+  // Lock input
 }
+
+function validateGuess(){
+  let guess = parseInt(guessInput.value)
+  console.log(guess, min, max)
+
+  // Validate input
+  if (isNaN(guess) || guess < min || guess > max){
+    setMessage('Please enter a number between 1 and 10.', 'red')
+    guessInput.value = ''
+  } else {
+    checkGuess(guess)
+  }
+}
+
+// Check guess
+function checkGuess(guess){
+  if (guess === winningNumber){
+    gameOver(true, `Woop! Woop! ${winningNumber} is correct!`)
+  } else {
+    guessesLeft -= 1
+    // Check for remaining guesses
+    if (guessesLeft === 0){
+      gameOver(false, `Womp womp. You are out of guesses. The correct number was ${winningNumber}. Better luck next time!`)
+    } else {
+      guessInput.style.borderColor = 'red'
+      guessInput.value = ''
+      setMessage(`${guess} is incorrect. You have ${guessesLeft} guesses remaining.`, 'red')
+    }
+  }
+}
+  
+// Create setMessage function 
+function setMessage(msg, color){
+  message.textContent = msg
+  message.color = color
+}
+// Create gameOver function
